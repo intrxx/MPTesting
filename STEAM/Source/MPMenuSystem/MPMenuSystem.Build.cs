aaf5ae3ca679d19305b4998995553c998ca1c4cc
intrxx <michaloginski15@gmail.com>
2023-08-17 21:37:52 +0200

    Creating Steam Session

---
+++ STEAM/Source/MPMenuSystem/MPMenuSystem.Build.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MPMenuSystem : ModuleRules
+{
+	public MPMenuSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", 
+			"HeadMountedDisplay", "EnhancedInput", "OnlineSubsystemSteam", "OnlineSubsystem" });
+	}
+}
